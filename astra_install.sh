bb022a64073dbcd41b4e04daa94817b8804fac6c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-05-06 17:52:38 +0200

    Making everything work (#1)
    * added reqs file
    
    * added new reqs
    
    * added help on astra installation
    
    * added the experiments notebooks and logs
    
    * last logs update

---
+++ astra_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install build-essential libtool autoconf automake libboost-dev
+g clone git@github.com:astra-toolbox/astra-toolbox.git
+cd build/linux
+sh autogen.sh
+sh configure --with-cuda=/usr/local/cuda \
+            --with-python=python \
+            --with-install-type=module
